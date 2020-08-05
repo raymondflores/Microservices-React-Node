5ea270281a1038d31e121068d9d984eb67c17e36
Raymond Flores <raymond@raymonds-mbp.lan>
2020-08-04 21:33:49 -0500

    dockerize posts

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -22,19 +22,38 @@ app.post('/posts/:id/comments', async (req, res) => {
   const { id } = req.params
 
   const comments = commentsByPostId[id] || []
-  comments.push({ id: commentId, content })
+  comments.push({ id: commentId, content, status: 'pending' })
   commentsByPostId[id] = comments
 
   await axios.post('http://localhost:4005/events', {
     type: 'CommentCreated',
-    data: { id: commentId, content, postId: id }
+    data: { id: commentId, content, postId: id, status: 'pending' }
   })
 
   res.status(201).send(comments)
 })
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Received Event', req.body.type)
+  const { type, data } = req.body
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data
+    const comments = commentsByPostId[postId]
+
+    const comment = comments.find(comment => comment.id === id)
+    comment.status = status
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content
+      }
+    })
+  }
 
   res.send({})
 })
